1162b7e2bac69cf7e7fff362808b86d5c4ced2cf
AlexDest13 <perseus.ua@gmail.com>
2019-10-02 23:14:54 +0300

    hw02 s1

---
+++ HW02/src/lits/hw02.java
@@ -0,0 +1,81 @@
+package lits;
+
+import java.util.Random;
+
+public class hw02 {
+        public static void main(String[] args) {
+
+// завдання 1
+// Просумувати парні числа від 1 до 15
+//
+//        int sum = 0;
+//        for (int i = 1; i <= 15; i++) {
+//            if (i % 2 == 0) {
+//                sum += i;
+//            }
+//        }
+//        System.out.println(sum);
+
+
+// завдання 2
+// Згенерувати рандомний рядок chars. Потірбно вивести в рядок 10 випадкових char символів.
+//
+//    int from = 0;
+//    int to = 255;
+//    char rez = ' ';
+//        for (int i = 0; i < 10; i++) {
+//            rez = (char) (from + (int) (Math.random() * to));
+//            System.out.print(rez);
+//        }
+
+
+// завдання 3
+// Взяти випадкове число, перевірити його і якшо воно
+// ділиться на 3 - то вивести шо діл на 3, якшо на 5 - то дів на 5 - якшо на 7 - то вивести шо дів на 7
+//
+//
+//        var randomNumber = new Random().nextInt(1000);
+//            if (randomNumber % 3 == 0) {
+////                System.out.println("Число " + randomNumber + " ділиться на 3");
+//                System.out.printf("Число %d ділиться на 3 \n", randomNumber);
+
+//            }
+//            else if (randomNumber % 5 == 0) {
+////                System.out.println("Число ділиться на 5");
+//                System.out.printf("Число %d ділиться на 5 \n", randomNumber);
+
+//            }
+//            else if (randomNumber % 7 == 0) {
+////                System.out.println("Число ділиться на 7");
+//                System.out.printf("Число %d ділиться на 7 \n", randomNumber);
+//            }
+
+
+// завдання 4
+// Зробити цикл на 50 повторень і генерувати випадкові числа, вивести і просумувати числа які націло діляться на 7
+//
+//            int sum = 0;
+//            for (int i = 0; i < 50; i++) {
+//                var randomNumber = new Random().nextInt(100);
+//                if (randomNumber % 7 == 0) {
+//                    sum +=randomNumber;
+//                }
+//                System.out.println(randomNumber);
+//            }
+//        System.out.println(sum);
+
+// завдання 5
+// просумувати 15 випадкових чисел і знайти їх середнє значеня
+//
+//
+//            var randomNumber = new Random().nextInt(50);
+//            int sum = 0;
+//            if ()
+
+
+
+
+
+
+        }
+    }
